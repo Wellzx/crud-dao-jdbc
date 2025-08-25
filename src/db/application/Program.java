9774001a5d3a8e7d37574397c55ab48d3203e7d4
Wel <welissonf928@gmail.com>
2025-08-25 18:41:40 -0300

    Create SellerDaoJDBC and DaoFactory

--- src/db/application/Program.java
+++ src/db/application/Program.java
@@ -1,5 +1,7 @@
 package db.application;
 
+import model.dao.SellerDao;
+import model.dao.impl.DaoFactory;
 import model.entities.Department;
 import model.entities.Seller;
 
@@ -10,6 +12,8 @@ public class Program {
         Department obj = new Department(1, "Books");
 
         Seller seller = new Seller(13, "Rosane", "rosa@gmail.com", new Date(), 1500.0, obj);
+
+        SellerDao sellerDao = DaoFactory.createSellerDao();
 
         System.out.println(seller);
     }
---
+++ src/model/dao/impl/DaoFactory.java
@@ -0,0 +1,10 @@
+package model.dao.impl;
+
+import model.dao.SellerDao;
+
+public class DaoFactory {
+
+    public static SellerDao createSellerDao(){
+        return new SellerDaoJDBC();
+    }
+}
