7d0917ff727669f60b25b7311d3908424580aeea
Wel <welissonf928@gmail.com>
2025-08-24 22:02:19 -0300

    Create seller entity class

--- src/model/entities/Department.java
+++ src/model/entities/Department.java
@@ -5,10 +5,11 @@ import java.io.Serializable;
 import java.util.Objects;
 
 public class Department implements Serializable {
+
     @Serial
     private static final long serialVersionUID = 1L;
 
-    private Integer Id;
+    private Integer id;
     private String name;
 
     public Department(){
@@ -16,16 +17,16 @@ public class Department implements Serializable {
     }
 
     public Department(Integer id, String name) {
-        Id = id;
+        this.id = id;
         this.name = name;
     }
 
     public Integer getId() {
-        return Id;
+        return id;
     }
 
     public void setId(Integer id) {
-        Id = id;
+        this.id = id;
     }
 
     public String getName() {
@@ -40,18 +41,18 @@ public class Department implements Serializable {
     public boolean equals(Object o) {
         if (o == null || getClass() != o.getClass()) return false;
         Department that = (Department) o;
-        return Objects.equals(Id, that.Id);
+        return Objects.equals(id, that.id);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hashCode(Id);
+        return Objects.hashCode(id);
     }
 
     @Override
     public String toString() {
         return "Department{" +
-                "Id=" + Id +
+                "id=" + id +
                 ", name='" + name + '\'' +
                 '}';
     }
